8555ad946d37d481eff7dc2140185118bcec914e
iamaniket143 <aniket.pantawne.ap@gmail.com>
2025-02-17 21:26:52 +0530

    Episode02 started

--- Episode02/App.js
+++ Episode02/App.js
@@ -11,7 +11,9 @@
 //     child1
 //     child2
 
-const heading1 = React.createElement("h1",{style:{color:"red"}},"Hello World"); 
+import React from "react";
+import ReactDOM from "react-dom/client";
+const heading1 = React.createElement("h1",{id: "heading"},"Aniket"); 
 const heading2 = React.createElement("h2",{style:{color:"red"}},"Hello World");
 const child1 = React.createElement("div",{id:"child1"},[heading1,heading2])
 const child2 = React.createElement("div",{id:"child2"},[heading1,heading2])
