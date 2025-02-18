e2d8b6cbb5dfb1a1593b209b6d215dff5c5f01fe
iamaniket143 <aniket.pantawne.ap@gmail.com>
2025-02-18 18:34:41 +0530

    notes file added

--- Episode03/Coding/App.js
+++ Episode03/Coding/App.js
@@ -10,4 +10,33 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 // h1 tag inside JSX
 const headingJSX = <h1>Created using JSX</h1>;
 
-root.render(headingJSX);
+// React Component (JS Function)
+
+function HeadingComponent1() {
+    return <h1>React Component</h1>
+}
+
+
+const HeadingComponent2 = () => {
+    return <h1>React Component Arrow</h1>}
+
+    
+const HeadingComponent2a = () => <h1>React Component Arrow Short</h1>
+
+const number = 101;
+
+const HeadingComponent2b = () => (
+    <h1>React Component Arrow2b {number}</h1>
+)
+
+
+const HeadingComponent3 = function()  {
+    return <h1>React Component Arrowas</h1>
+}
+
+const HeadingComponent2c = () => (
+    <p>{number}</p>
+)
+
+root.render(<HeadingComponent2c />);
+
