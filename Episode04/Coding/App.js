1894f5b2b1bbb9b563540b8c5ec728ed7999e94b
iamaniket143 <aniket.pantawne.ap@gmail.com>
2025-02-18 21:22:53 +0530

    Episode 4 added

---
+++ Episode04/Coding/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const Heading = () => <h1>Aniket</h1>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Heading />);
